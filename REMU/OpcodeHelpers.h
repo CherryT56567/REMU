c4622f457e7abcff3815ab950d6a7a75d14184f1
Ronit Dsilva <160979154+CherryT56567@users.noreply.github.com>
2024-09-05 08:44:34 +1000

    Update OpcodeHelpers.h

--- REMU/OpcodeHelpers.h
+++ REMU/OpcodeHelpers.h
@@ -8,7 +8,7 @@ uint64_t imm_S(uint64_t inst);
 uint64_t imm_U(uint64_t inst);
 uint64_t imm_J(uint64_t inst);
 uint64_t imm_B(uint64_t inst);
-uint32_t shamt(uint64_t inst);
+uint64_t shamt(uint64_t inst);
 uint64_t csr(uint64_t inst);
 uint64_t wrapping_shr(uint64_t value, unsigned int shift);
 uint64_t wrapping_shl(uint64_t value, unsigned int shift);
